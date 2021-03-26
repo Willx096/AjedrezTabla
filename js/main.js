88c983d18b9796fe5da738031ce9cbbca4411046
Willx096 <guillem.lorca96@gmail.com>
2021-03-26 22:29:11 +0100

    primerasubida

---
+++ js/main.js
@@ -0,0 +1,18 @@
+var createChessTable = function (rows,cols){
+	var i = 0;
+	var j = 0;
+	var table = '<table>' ;
+
+	if (i < rows){
+		
+	}
+
+} 
+
+
+chessTable = createChessTable (4,4);
+
+document.getElementById('container').innerHTML = chessTable;
+
+
+
